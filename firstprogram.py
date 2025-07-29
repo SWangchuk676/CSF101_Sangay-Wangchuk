a397b85c8a603b2b8aac58670c6de43241291f1d
swangchuk676 <02250030>
2025-07-29 14:36:09 +0600

    Second Commit

--- firstprogram.py
+++ firstprogram.py
@@ -1,1 +1,7 @@
-print("Hello SWE")
+a=2
+b=3
+sum=a+b
+print("The sum of a and b is: ",sum )
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("you are",name,"and you are",age,"years old")
