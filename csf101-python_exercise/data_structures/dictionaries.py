8ca5c6b35a06f66aaf04db2cfc5f4605fa90239d
swangchuk676 <02250030>
2025-08-19 21:38:52 +0600

    python dictionaries.py

--- csf101-python_exercise/data_structures/dictionaries.py
+++ csf101-python_exercise/data_structures/dictionaries.py
@@ -1,1 +1,17 @@
-name = "John Doe" 
+name = "Sangay Wangchuk"
+age = 19
+height = 1.75
+is_student = True
+person_info = {
+    "name": name,
+    "age": age,
+    "height": height,
+    "is_student": is_student
+}
+print(person_info)
+person_info["favorite_color"] = "blue"
+print(person_info)
+try:
+    print(person_info["weight"])
+except KeyError as e:
+    print(f"Error: {e}")
